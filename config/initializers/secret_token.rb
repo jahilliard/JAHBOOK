fafc0d4f4051244bbd20ed06abef66aa23a74c17
Justin Hilliard <justin.a.hilliard@gmail.com>
2014-01-21 16:20:50 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jahbook::Application.config.secret_token = 'a31176ac0a91b3762bb71aa3d4692d638e1e8160334a92485fae30f73184240bd10f6a9dbc1f0984888c10b91c22712d29a639259478586a90576744227b5763'
